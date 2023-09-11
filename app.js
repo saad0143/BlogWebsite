146c878e1c6ba9531ca5a8dbec514ba92acc578a
Saad Anwar <01saadanwar@gmail.com>
2023-09-11 12:39:55 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const blogRoutes=require('./routes/blogRoutes')
+const mongoose = require('mongoose');
+
+
+// express app
+const app = express();
+
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://saad:saad123@cluster0.hxdannt.mongodb.net/saadblog?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    app.listen(3000);
+    console.log('connected'); 
+  })
+  .catch(err => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+//blog routes
+app.use('/blogs',blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
